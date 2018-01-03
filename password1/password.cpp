fb4ba116b8c73f1fac77718f637ef3091f5b04fa
meichendavid <meichendavid@users.noreply.github.com>
2018-01-03 13:23:15 -0600

    Added algorithm for translating random number into a random char with equal probability

--- password1/password.cpp
+++ password1/password.cpp
@@ -32,8 +32,65 @@ Password::Password(int l, int a): length(l)
 	}
 }
 
+/* Also known as psychopath method */
 void Password::random(int l)
 {
+	/* Char set used (ASCII):
+	 * 0 - 9 (48 - 57) 10      0 - 9
+	 * a - z (97 - 122) 26     10 - 35
+	 * A - Z (65 - 90) 26      36 - 61
+	 * Assorted symbols (in an array)
+	 * 33 (!), 35 (#), 36 ($), 37 (%), 38 (&), 63(?), 64(@) 7 62 - 68
+	 */
+
+	/* A random seed will draw a number from [0, 74], then an algorithm figures out which char to get */
+
+	/* Random number [0, 74] */
+	srand(time(NULL));
+	int place = rand() % 75;
+	char c;
+
+	if (place < 74 && place >= 62)
+	{
+		switch(place)
+		{
+			case 62:
+				c = '!';
+				break;
+			case 63:
+				c = '#';
+				break;
+			case 64:
+				c = '$';
+				break;
+			case 65:
+				c = '%';
+				break;
+			case 66:
+				c = '&';
+				break;
+			case 67:
+				c = '?';
+				break;
+			case 68:
+				c = '@';
+				break;
+		}
+	}
+	else if (place >= 36)
+	{
+		c = (char) place - 36 + 'A';
+	}
+	else if (place >= 10)
+	{
+		c = (char) place - 10 + 'a';
+	}
+	else if (place < 10)
+	{
+		c = (char) place + '0';
+	}
+
+	cout << c << endl;
 }
 
 void Password::schneier(int l)
@@ -50,5 +107,7 @@ void Password::pao(int l)
 
 int main()
 {
+	Password *p = new Password(0, 0);
+
 	return 0;
 }
