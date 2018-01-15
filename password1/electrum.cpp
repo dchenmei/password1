02cdddefbab056c7effd1a954f8982380a5a8986
swolewizard <swolewizard@users.noreply.github.com>
2018-01-15 17:12:56 -0600

    Added comments to electrum method

--- password1/electrum.cpp
+++ password1/electrum.cpp
@@ -1,14 +1,22 @@
+/***********************************************
+ *
+ * Implements Electrum password
+ *
+ * Author: swolewizard
+ *
+ ***********************************************/
+
 #include "electrum.hpp"
 
 Electrum::Electrum()
 {
-/* with spaces because that take longer to crack, length is king sometimes */
     vector<string> seeds = read_seeds();
     
     /* Randomize and mix up the words */
     srand(time(NULL));
     random_shuffle(seeds.begin(), seeds.end());
     
+	/* Create a new sequence of words */
 	string temp = "";
     for (string seed : seeds)
     {   
@@ -20,14 +28,11 @@ Electrum::Electrum()
 
 vector<string> Electrum::read_seeds() 
 {
-	/* No repeats allowed & no words less than three characters */
-    /* TODO: NO MAGIC NUMBERS */
-    
     cout << "Please enter twelve words (3 char min)." << endl;
     
     vector<string> words;
     string s;
-    for (int i = 0; i < 12; ++i)
+    for (int i = 0; i < NUM_SEEDS; ++i)
     {   
         cin >> s;
         if (s.size() < 3)
--- password1/electrum.hpp
+++ password1/electrum.hpp
@@ -1,13 +1,25 @@
+/***********************************************
+ *
+ * Defines Electrum password, generated using 
+ * 12 words as seeds. Similar method also used
+ * in bitcoin wallet passwords.
+ *
+ * Author: swolewizard
+ *
+ ***********************************************/
 #ifndef ELECTRUM_HPP
 #define ELECTRUM_HPP
 #include "password.hpp"
+#define NUM_SEEDS 12
 
 class Electrum: public Password
 {
   public:
+	/* With twelve seeds (words) from users, create a new sequence of words*/
 	Electrum();
 
   private:
+	/* Read 12 words from user as seeds */
 	vector<string> read_seeds();
 };
 
