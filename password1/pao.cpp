473764aca8597c82ee23e07f4df34b73a7f8d831
swolewizard <swolewizard@users.noreply.github.com>
2018-01-15 20:56:53 -0600

    Updated PAO class and removed extra helper methods

--- password1/pao.cpp
+++ password1/pao.cpp
@@ -1,54 +1,38 @@
+/***********************************************
+ * Implements PAO password
+ *
+ * Author: swolewizard
+ ***********************************************/
+
 #include "pao.hpp"
 
 PAO::PAO()
 {
- vector<string> words;
+    /* Mnemonic Sentence:
+     * [Adjective] [celebrity] [verb] a [adjective] [brand] [object] at [adjective] [famous place] on [month or holiday] 
+     */
 
-    /* Adjective celebrity verb 'a' adjective brand object 'at' adjective famous place 'on' month or holiday */
-    
-    cout << "Enter an adjective:" << endl;
-    words.push_back(get_word());
-
-    cout << "Enter a celebrity:" << endl;
-    words.push_back(get_word());
-
-    cout << "Enter a verb:" << endl;
-    words.push_back(get_word());
-
-    words.push_back("a");
-    
-    cout << "Enter an adjective:" << endl;
-    words.push_back(get_word());
-
-    cout << "Enter a brand:" << endl;
-    words.push_back(get_word());
-
-    cout << "Enter an object:" << endl;
-    words.push_back(get_word());
-
-    words.push_back("at");
-
-    cout << "Enter an adjective:" << endl;
-    words.push_back(get_word());
-
-    cout << "Enter a famous place:" << endl;
-    words.push_back(get_word());
-
-    words.push_back("on");
-
-    cout << "Enter a month (in word)  or holiday:" << endl;
-    words.push_back(get_word());
-
+	/* Because the instructions are the same ("Enter ..."), it is a pefect opportunity to use a loop! */
+	vector<string> words = { "adjective", "celebrity", "verb", "adjective", "brand", "object", "adjective", "famous place", "month or holiday" };
 	string temp = "";
-    for (string word : words)
-        temp += (word + " ");
+	string w;
+
+	int i = 0;
+	for (string word : words)
+	{
+		if (i == 3)
+			temp += "a ";
+		else if (i == 6)
+			temp += "at ";
+		else if (i == 8)
+			temp += "on ";
+
+		cout << "Enter " << word << ": ";
+		if (cin >> w)
+			temp += (w + " ");
+
+		++i;
+	}
 
 	set_password(temp);
-}
-
-string PAO::get_word()
-{
-	string s;
-    getline(cin, s);
-    return s;
 }
--- password1/pao.hpp
+++ password1/pao.hpp
@@ -1,3 +1,12 @@
+/***********************************************
+ * Describes PAO password, came from some book
+ * I read. It utilizes mnemonic techniques to 
+ * create a strong password that can be easily 
+ * remembered.
+ *
+ * Author: swolewizard
+ ***********************************************/
+
 #ifndef PAO_HPP
 #define PAO_HPP
 #include "password.hpp"
@@ -6,9 +15,6 @@ class PAO: public Password
 {
   public:
 	PAO();
-
-  private:
-	string get_word();
 };
 
 #endif
